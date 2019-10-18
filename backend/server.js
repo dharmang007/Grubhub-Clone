91e551b3774690f4441df6d0ed7f56a59169a4fc
Dharmang Solanki <dharmangsolanki999@gmail.com>
2019-10-17 19:32:40 -0700

    Added the database connection

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,17 @@
+/************* Import Statements ***************/
 var Customer = require('./controllers/customers');
 var Restaurant = require('./controllers/restuarants');
-
-//import express module 
 var express = require('express');
-//create  an express app
-var app = express();
-//require express middleware body-parser
 var bodyParser = require('body-parser');
-//require express session
 var session = require('express-session');
-
+const connectToDataBase = require("./config/db");
 var cookieParser = require('cookie-parser');
 var { check, validationResult } = require('express-validator');
-var mysql = require('mysql');
-//const port = process.env.PORT || 3001;
-const port =  3001;
-const connection = mysql.createConnection({
-    host:"localhost",
-    user:'root',
-    password:'root',
-    database: 'grubhub'
-});
-
-//specify the path of static directory
-app.use(express.static(__dirname + './public')); 
-//use body parser to parse JSON and urlencoded request bodies
+/***********************************************/
+const port = process.env.PORT || 3001;
+var app = express();
 app.use(bodyParser.json());
-
-//app.use(bodyParser.urlencoded({ extended: true })); 
-
+app.use(cookieParser());
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
@@ -37,17 +20,13 @@ app.use(function(req, res, next) {
     res.setHeader('Cache-Control', 'no-cache');
     next();
 })
-
-//use cookie parser to parse request headers
-app.use(cookieParser());
-//use session to store user data between HTTP requests
-
 app.use(session({
     secret: 'Nothing',
     resave: false,
     saveUninitialized: true
   }));
 
+connectToDataBase();
 app.get('/', (req,res)=>{
     res.end("Server is Working!!");
 })
@@ -72,9 +51,7 @@ app.get('/home',(req,res)=>{
 //This method adds new users in the database
 app.post('/create-user',(req,res)=>Customer.default.signUp(connection,req,res))
 app.post('/create-restaurant',(req,res)=>Restaurant.default.signUp(connection,req,res))
-
-app.get('/view-restaurants',(req,res)=>{
-    
+app.get('/view-restaurants',(req,res)=>{ 
     var getRestaurants = `SELECT * FROM restaurants;`;
     connection.query(getRestaurants,(err,rows,fields)=>{  
         if(!err){
@@ -85,7 +62,13 @@ app.get('/view-restaurants',(req,res)=>{
 
 
 app.listen(process.env.PORT||port, () =>{
-    console.log("Server running on host 3001")
+    if(process.env.PORT){
+        console.log("Server running on host "+ process.env.PORT.toString());
+    }
+    else{
+        console.log("Server running on host "+ port.toString());
+    }
+    
 })
 
 
