e687b954ab6c68202e3da14bec805968dc8a50a9
Dharmang Solanki <dharmangsolanki999@gmail.com>
2019-11-06 14:49:31 -0800

    Changed port

--- frontend/src/constants/defaultValues.js
+++ frontend/src/constants/defaultValues.js
@@ -1,4 +1,4 @@
 export default {
- //serverURI : 'ec2-3-82-112-45.compute-1.amazonaws.com:3001'
- serverURI : "http://localhost:3001"
+ serverURI : 'ec2-3-82-112-45.compute-1.amazonaws.com:3001'
+ //serverURI : "http://localhost:3001"
 }
